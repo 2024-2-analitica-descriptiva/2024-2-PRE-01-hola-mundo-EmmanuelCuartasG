7e51504c1ac9021ae261a5b3de372689f7b7cdce
Emmanuel Cuartas Giraldo <ecuartasg@unal.edu.co>
2024-11-11 23:37:04 -0500

    prueba_laptop

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,7 +3,7 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
 # pylint: disable=import-outside-toplevel
-
+#
 
 def pregunta_01():
     """
@@ -14,7 +14,7 @@ def pregunta_01():
 
     """
 
-    return "Hola mundo cruel!"
+    return
 
 
 if __name__ == "__main__":
